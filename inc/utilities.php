12383ba35a7683f123dbaba1debfde502f22d886
Aidan <aidan.b.mcarthur@gmail.com>
2018-09-26 16:03:51 -0700

    Fix node traversal

--- inc/utilities.php
+++ inc/utilities.php
@@ -19,12 +19,14 @@
 
 if (!defined('ABSPATH')) die("Forbidden");
 
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\StaticCall;
 use PhpParser\ParserFactory;
 use PhpParser\Error;
 use PhpParser\Node;
 use PhpParser\NodeFinder;
 use PhpParser\PrettyPrinter;
-use PhpParser\NodeDumper;
 
 function compatibuddy_get_plugins() {
 
@@ -80,32 +82,56 @@ function compatibuddy_get_php_files_in_directory( $directory ) {
     return $files;
 }
 
+function compatibuddy_deep_expression_search ( $node, $function_name, &$found_node ) {
+
+    if ( ( $node instanceof FuncCall || $node instanceof StaticCall || $node instanceof MethodCall )
+        && $node->name == $function_name ) {
+        $found_node = $node;
+        return true;
+    }
+
+    if ( isset( $node->expr ) ) {
+       //return compatibuddy_deep_expression_search( $node->expr, $function_name, $found_node );
+    }
+
+    return false;
+}
+
+function compatibuddy_func_call_filter( $node, $function_name ) {
+    $found_node = null;
+    compatibuddy_deep_expression_search( $node, $function_name, $found_node );
+    return $found_node;
+}
+
 function compatibuddy_get_function_calls( $file, $function_name ) {
 
     $contents = file_get_contents( $file );
 
     $prettyPrinter = new PrettyPrinter\Standard;
-    $nodeFinder = new NodeFinder;
+
     $parser = (new ParserFactory)->create( ParserFactory::PREFER_PHP7 );
     $function_calls = array();
+
     try {
         $calls = array();
         $ast = $parser->parse($contents);
+        $nodeFinder = new NodeFinder;
         $calls[] = $nodeFinder->find($ast, function( Node $node ) use( $function_name ) {
-            return $node instanceof Node\Stmt\Expression
-                && $node->expr->name->parts[0] === $function_name;
+            return ($node instanceof FuncCall || $node instanceof StaticCall || $node instanceof MethodCall)
+                && ($node->name instanceof PhpParser\Node\Name) && $node->name->toString() == $function_name;
         });
 
         foreach ( $calls as $expressions ) {
             foreach ( $expressions as $expression ) {
                 $args = array();
-                foreach ( $expression->expr->args as $arg ) {
+                foreach ( $expression->args as $arg ) {
                     $args[] = $prettyPrinter->prettyPrint(array($arg));
                 }
 
-                $function_calls[$args[0]] = array(
+                $function_calls[] = array(
                     'args' => $args,
-                    'line' => $expression->getStartLine()
+                    'line' => $expression->getLine(),
+                    'name' => $expression->name
                 );
             }
         }
